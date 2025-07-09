0e0bae3621b25ae2115018ff928a9eb98dbaf08b
Tisitha <tisitha007@gmail.com>
2025-07-09 22:36:57 +0530

    first commit

---
+++ src/main/java/com/tisitha/weather_based_travel_helper/WeatherBasedTravelHelperApplication.java
@@ -0,0 +1,13 @@
+package com.tisitha.weather_based_travel_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBasedTravelHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherBasedTravelHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tisitha/weather_based_travel_helper/WeatherBasedTravelHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tisitha.weather_based_travel_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBasedTravelHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
